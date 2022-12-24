7a95494db88a2408405d01721a59b8bde4ace9cb
Suza <zabaniya101@gmail.com>
2022-12-24 18:02:49 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,42 @@
+<div align="center">
+  <h1><code>Google photos date merger</code></h1>
+  <p>
+    <strong>Apply the correct dates to exported google photos.</strong>
+  </p>
+  <p style="margin-bottom: 0.5ex;">
+    <img
+        src="https://img.shields.io/github/downloads/zabaniya001/google-photos_date_merger/total"
+    />
+    <img
+        src="https://img.shields.io/github/last-commit/zabaniya001/google-photos_date_merger"
+    />
+    <img
+        src="https://img.shields.io/github/issues/zabaniya001/google-photos_date_merger"
+    />
+    <img
+        src="https://img.shields.io/github/issues-closed/zabaniya001/google-photos_date_merger"
+    />
+    <img
+        src="https://img.shields.io/github/repo-size/zabaniya001/google-photos_date_merger"
+    />
+    <img
+        src="https://img.shields.io/github/workflow/status/zabaniya001/google-photos_date_merger/Compile%20and%20release"
+    />
+  </p>
+</div>
+
+
+## Requirements ##
+- [Filedate](https://pypi.org/project/filedate/)
+
+## Installation ##
+1. Download the script
+2. Download the dependencies ( go to the cmd and type `pip install filedate`)
+
+
+## Usage ##
+1) Open the cmd
+2) Go to the path of the script ( `cd <folder where the script resides in>` )
+3) Write in the cmd `py main.py`
+4) Paste the folder where the exported google photos are in.
+5) Type `y` if you want to delete the json files ( images and the metadata, which are files that contain informations like when the image was created and such, are separate files and are not necessary once you merge the dates ).
---
+++ main.py
@@ -0,0 +1,60 @@
+import filedate
+import os
+import json
+import time
+from datetime import datetime
+
+def main():
+    # Get the folder where the the files are.
+    main_directory = input("Where is the folder where the extracted photos reside?")
+    delete_json = input("Would you like to delete the json files after merging the dates ( they're not needed once we merge the dates ) [ y/n ]")
+
+    if delete_json == "y":
+        delete_json = 1
+    else:
+        delete_json = 0
+
+    iterate_through_files(main_directory, delete_json)
+
+
+def iterate_through_files(directory, delete_json):
+    for filename in os.listdir(directory):
+        complete_path = os.path.join(directory, filename)
+
+        if os.path.isdir(complete_path):
+            iterate_through_files(complete_path)
+
+        if ".json" in filename:
+            continue
+
+        json_path = complete_path + ".json"
+
+        # Check if the .json of the file exists in the first place.
+        if not os.path.exists(json_path):
+            continue
+
+        print(complete_path)
+        
+        with open(json_path, "r", encoding="utf8") as json_file:
+            json_object = json.loads(json_file.read())
+        
+        if delete_json:
+            os.remove(json_path)
+        
+        creation_time = datetime.fromtimestamp(int(json_object["photoTakenTime"]["timestamp"]))
+        modified_time = datetime.fromtimestamp(int(json_object["photoLastModifiedTime"]["timestamp"]))
+
+        print("--Creation time: " + str(creation_time))
+        print("--Modified time: " + str(creation_time))
+        print("\n")
+
+        test = filedate.File(complete_path)
+
+        test.set(
+            created = creation_time,
+            modified = modified_time
+        )
+
+
+if __name__ == '__main__':
+    main()
